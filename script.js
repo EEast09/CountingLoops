a5f6cb23658946cda717740c0cbdf73b899db231
Erinn East <erinn.east@gmail.com>
2022-07-11 21:48:58 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+for (let i = 1; i < 11; i++) {
+    console.log(i)
+  }
+
+  for (let i = 1; i > 11; i--) {
+    console.log(i)
+  }
+
+ 
+  let i = 0;
+  while (i < 10) {
+    i++;
+    console.log(i)
+  } 
+
+  let j = 0;
+  while (j > 10) {
+    j--;
+    console.log(j)
+  } 
+
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+for(let i=0; i< numbers.length; i++){
+  console.log(numbers[i])
+}
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
